b098f64011ecd44595d7051acd0aee8bd3c821c6
braullio <braulliovg@gmail.com>
2025-11-03 11:42:36 -0300

    feat(project): start spring io

---
+++ src/main/java/io/github/braullio/oraclebase/SpringBootOracleBaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.braullio.oraclebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOracleBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOracleBaseApplication.class, args);
+	}
+
+}
